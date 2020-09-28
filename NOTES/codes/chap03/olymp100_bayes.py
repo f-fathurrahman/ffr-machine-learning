59c5336fafc1a6c69abb1297a76c221e30f79091
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-28 10:42:12 +0700

    Using scipy.stats to evaluate mvn pdf

--- NOTES/codes/chap03/olymp100_bayes.py
+++ NOTES/codes/chap03/olymp100_bayes.py
@@ -1,12 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.stats
 from numpy.linalg import inv
 
 import matplotlib
 matplotlib.style.use("ggplot")
 
 np.random.seed(1234)
+
+
+def eval_gauss2d(μ, Σ, x, y):
+    D = np.linalg.det(Σ)
+    Sinv = np.linalg.inv(Σ)
+    w = np.array([x, y])
+    wmu = w - μ
+    C1 = np.matmul(Σ, wmu)
+    C2 = -0.5*np.dot(wmu, C1)
+    return np.exp(C2)/(2*np.pi*np.sqrt(D))
+
+def plot_gauss_contour(μ, Σ, xlim, ylim, Nx=101, Ny=101, filesave="IMG_contour.png"):
+    #
+    x = np.linspace(xlim[0], xlim[1], Nx)
+    y = np.linspace(ylim[0], ylim[1], Ny)
+    Z = np.zeros((Nx,Ny))
+    mvn = scipy.stats.multivariate_normal(μ, Σ)
+    # Evaluate by loop
+    for i in range(Nx):
+        for j in range(Ny):
+            Z[i,j] = mvn.pdf([ x[i], y[j] ])
+    plt.clf()
+    plt.contour(x, y, np.transpose(Z), 20)
+    #plt.axis("equal")
+    plt.savefig(filesave, dpi=150)
+
 
 # x: input/feature vector
 # Norder: polynomial order
@@ -35,8 +61,8 @@ x = 0.25*x
 # Define the prior
 # $p(\mathbf{w}) = {\cal N}(\mu_0,\Sigma_0)
 μ_0 = np.array([0.0, 0.0])
-Σ_0 = np.array([ [100, 0], [0.0, 5.0] ])
-σ2 = 0.05 # Vary this to see the effect on the posterior samples
+Σ_0 = np.array([ [100, 0.0], [0.0, 5] ])
+σ2 = 2.0 # Vary this to see the effect on the posterior samples
 
 # Draw some functions from the prior
 W = np.random.multivariate_normal(μ_0, Σ_0, 10)
@@ -59,6 +85,9 @@ plt.xlim(xlim)
 plt.ylim(ylim)
 plt.savefig("IMG_sample_01.pdf")
 
+plot_gauss_contour(μ_0, Σ_0, [9, 13], [-0.2, 0.2],
+    filesave="IMG_contour_0.png")
+
 # Add the data 3 points at a time
 NsamplesTry = np.arange(3,27+1,3)
 for Nsample in NsamplesTry:
@@ -72,18 +101,16 @@ for Nsample in NsamplesTry:
     tmp1 = np.matmul(Xsub.transpose(), tsub)
     tmp2 = np.matmul(inv(Σ_0), μ_0)
     tmp3 = (1.0/σ2) * tmp1 + tmp2
-    #print("tmp1 = ", tmp1)
-    #print("tmp2 = ", tmp2)
     μ_w = np.matmul(Σ_w, tmp3)
     #
     plt.clf()
     plt.plot(x[Nsample:], t[Nsample:], marker="o", linestyle="None", label="Unused data")
     plt.plot(x[:Nsample], t[:Nsample], marker="o", linestyle="None", label="Used data")
     #
-    #print(type(Σ_w))
-    print("Σ_w = ", Σ_w)
-    #print(type(μ_w))
-    print("μ_w = ", μ_w)
+    print("\nNsample = ", Nsample)
+    print("μ_w = "); print(μ_w)
+    print("Σ_w = "); print(Σ_w)
+    #
     W = np.random.multivariate_normal(μ_w, Σ_w, 10)
     for i in range(10):
         w = W[i,:]
@@ -95,3 +122,6 @@ for Nsample in NsamplesTry:
     plt.legend()
     plt.savefig("IMG_sample_used_" + str(Nsample) + ".pdf")
     plt.savefig("IMG_sample_used_" + str(Nsample) + ".png", dpi=150)
+    #
+    plot_gauss_contour(μ_w, Σ_w, [9, 13], [-0.2, 0.2],
+        filesave="IMG_contour_" + str(Nsample) + ".png")
--- NOTES/codes/chap03/test_contour_gaussian_2.py
+++ NOTES/codes/chap03/test_contour_gaussian_2.py
@@ -1,14 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def eval_gauss2d(μ, Σ, x, y):
-    D = np.linalg.det(Σ)
-    Sinv = np.linalg.inv(Σ)
-    w = np.array([x, y])
-    wmu = w - μ
-    C1 = np.matmul(Σ, wmu)
-    C2 = -0.5*np.matmul(wmu.transpose(), C1)
-    return np.exp(C2)/(2*np.pi*np.sqrt(D))
+import scipy.stats
 
 μ = np.array([0.0, 0.1])
 Σ = np.array([[5.0, 0], [0.0, 5.0]])
@@ -19,12 +12,14 @@ x = np.linspace(-0.5, 0.5, Nx)
 y = np.linspace(-0.5, 0.5, Ny)
 Z = np.zeros((Nx,Ny))
 
+mvn = scipy.stats.multivariate_normal(μ, Σ)
+
 # Evaluate by loop
 for i in range(Nx):
     for j in range(Ny):
-        Z[i,j] = eval_gauss2d(μ, Σ, x[i], y[j])
+        Z[i,j] = mvn.pdf([x[i], y[j]])
 
 plt.clf()
 plt.contour(x, y, np.transpose(Z), 20)
-plt.savefig("IMG_test_contour.pdf")
+plt.savefig("IMG_test_contour_2.pdf")
 
