60e2d7d2e3b836cea968e1d8fa8b09b536140888
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-06-17 09:50:13 +0700

    Refactoring slide. Moving old python codes.

--- NOTES/LinearModel_LSQ/codes/linear_model/fit_polyN_olympic100.py
+++ NOTES/LinearModel_LSQ/codes/linear_model/fit_polyN_olympic100.py
@@ -1,5 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use("seaborn-darkgrid")
+#matplotlib.style.use("ggplot")
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.size": 14}
+)
 
 data = np.matrix( np.loadtxt("../DATA/olympic100m.txt", delimiter=",") )
 
@@ -32,13 +40,15 @@ def do_fit(Npoly):
     t_pred = X*w
 
     plt.clf()
-    plt.plot(x, t, marker="o", label="data", color="black")
-    plt.plot(x, t_pred, marker="o", lw=0, color="blue")
-    plt.plot(x_eval, t_eval, label="fitted", color="blue")
-    plt.grid()
+    plt.plot(x, t, marker="o", label="data")
+    #plt.plot(x, t_pred, marker="o", lw=0, label="predicted")
+    plt.plot(x_eval, t_eval, label="fitted")
+    plt.grid(True)
     plt.legend()
-    plt.savefig("TEMP_fit_poly" + str(Npoly) + "_olympic100.png", dpi=150)
+    plt.ylim(9.5, 12.2)
+    plt.xlim(-1, 30)
+    plt.savefig("IMG_fit_poly" + str(Npoly) + "_olympic100.png", dpi=150)
 
 for n in range(1,11):
     do_fit(n)
-    print("n = %d is done" % (n))
+    print("n = %2d is done" % (n))
