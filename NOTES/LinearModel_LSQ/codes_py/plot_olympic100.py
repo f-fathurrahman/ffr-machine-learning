92d1f66d3c8d4d7aa3c36437210b81b048dca8b8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-06-20 14:04:03 +0700

    Prepare to convert to Python

---
+++ NOTES/LinearModel_LSQ/codes_py/plot_olympic100.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.size": 14}
+)
+
+data = np.loadtxt("../../../DATA/olympic100m.txt", delimiter=",")
+
+t = data[:,1]
+x = data[:,0]
+
+plt.clf()
+plt.plot(x, t, marker="o", label="data")
+plt.grid(True)
+plt.legend()
+plt.xlabel("year")
+plt.ylabel("seconds")
+plt.savefig("IMG_data_olympic100m.png", dpi=150)
+plt.savefig("IMG_data_olympic100m.pdf")
