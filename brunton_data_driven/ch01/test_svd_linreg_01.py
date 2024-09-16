ecdff92581c8e8e9c49bc64b3f4886d17433b9a1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-09-16 18:45:48 +0700

    Some comment and reorganize

--- brunton_data_driven/ch01/test_svd_linreg_01.py
+++ brunton_data_driven/ch01/test_svd_linreg_01.py
@@ -6,12 +6,12 @@ plt.rcParams.update({"font.size": 18})
 x = 3 # True slope
 a = np.arange(-2,2,0.25)
 a = a.reshape(-1, 1)
-b = x*a + np.random.randn(*a.shape) # Add noise
+b = x*a + np.random.randn(*a.shape) # a linear function plus noise
 
 plt.plot(a, x*a, color="k", linewidth=2, label="True line") # True relationship
 plt.plot(a, b, "x", color="r", markersize = 10, label="Noisy data") # Noisy measurements
 
-U, S, VT = np.linalg.svd(a,full_matrices=False)
+U, S, VT = np.linalg.svd(a, full_matrices=False)
 xtilde = VT.T @ np.linalg.inv(np.diag(S)) @ U.T @ b # Least-square fit
 
 plt.plot(a,xtilde * a,"--",color="b",linewidth=4, label="Regression line")
@@ -24,8 +24,8 @@ plt.legend()
 plt.show()
 
 # Three methods of computing regression
-xtilde1 = VT.T @ np.linalg.inv(np.diag(S)) @ U.T @ b
-xtilde2 = np.linalg.pinv(a) @ b
+xtilde1 = VT.T @ np.linalg.inv(np.diag(S)) @ U.T @ b # using SVD result
+xtilde2 = np.linalg.pinv(a) @ b # using pinv function
 
 # Use sklearn?
 # xtilde3 = regress(b,a)
--- brunton_data_driven/ch01/test_svd_transform_01.py
+++ brunton_data_driven/ch01/test_svd_transform_01.py
@@ -5,30 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 plt.rcParams["figure.figsize"] = [16, 8]
 plt.rcParams.update({"font.size": 18})
 
-theta = np.array([np.pi/15, -np.pi/9, -np.pi/20])
-
-# A diagonal quantity
-Sigma = np.diag([3, 1, 0.5]) # scale x, then y, then z
-
-# Rotation about x axis
-Rx = np.array([[1, 0, 0],
-               [0, np.cos(theta[0]), -np.sin(theta[0])],
-               [0, np.sin(theta[0]), np.cos(theta[0])]])
-
-# Rotation about y axis
-Ry = np.array([[np.cos(theta[1]), 0, np.sin(theta[1])],
-               [0, 1, 0],
-               [-np.sin(theta[1]), 0, np.cos(theta[1])]])
-
-# Rotation about z axis
-Rz = np.array([[np.cos(theta[2]), -np.sin(theta[2]), 0],
-               [np.sin(theta[2]), np.cos(theta[2]), 0],
-               [0, 0, 1]])
-
-# Rotate and scale
-X = Rz @ Ry @ Rx @ Sigma
-
-
+# Prepare points for a sphere
 u = np.linspace(-np.pi, np.pi, 100)
 v = np.linspace(0, np.pi, 100)
 x = np.outer(np.cos(u), np.sin(v))
@@ -43,20 +20,53 @@ surf1 = ax1.plot_surface(
     x, y, z, cmap="jet", alpha=0.6,
     facecolors=plt.cm.jet(z),
     linewidth=0.5, rcount=30, ccount=30)
-
+box3d_aspect_ratio = (np.ptp(x), np.ptp(y), np.ptp(z))
+ax1.set_box_aspect(box3d_aspect_ratio)
 surf1.set_edgecolor("k")
 ax1.set_xlim3d(-2, 2)
 ax1.set_ylim3d(-2, 2)
 ax1.set_zlim3d(-2, 2)
 
+# Prepare transformation matrix
+
+def prepare_transform_matrix(thetax, thetay, thetaz):
+    # Define
+    #theta = np.array([np.pi/15, -np.pi/9, -np.pi/20])
+    theta = np.array([thetax, thetay, thetaz])
+    
+    # A diagonal quantity
+    Sigma = np.diag([1.0, 1.5, 1.0]) # scale x, then y, then z
+
+    # Rotation about x axis
+    Rx = np.array([[1, 0, 0],
+                   [0, np.cos(theta[0]), -np.sin(theta[0])],
+                   [0, np.sin(theta[0]), np.cos(theta[0])]])
+
+    # Rotation about y axis
+    Ry = np.array([[np.cos(theta[1]), 0, np.sin(theta[1])],
+                   [0, 1, 0],
+                   [-np.sin(theta[1]), 0, np.cos(theta[1])]])
+
+    # Rotation about z axis
+    Rz = np.array([[np.cos(theta[2]), -np.sin(theta[2]), 0],
+                   [np.sin(theta[2]), np.cos(theta[2]), 0],
+                   [0, 0, 1]])
+
+    # Rotate and scale
+    X = Rz @ Ry @ Rx @ Sigma
+
+    return X
+
+
+
+X = prepare_transform_matrix(0, 0, 0)
 xR = np.zeros_like(x)
 yR = np.zeros_like(y)
 zR = np.zeros_like(z)
-
 for i in range(x.shape[0]):
     for j in range(x.shape[1]):
         vec = [x[i,j], y[i,j], z[i,j]]
-        vecR = X @ vec
+        vecR = X @ vec # transform this point
         xR[i,j] = vecR[0]
         yR[i,j] = vecR[1]
         zR[i,j] = vecR[2]
@@ -66,6 +76,7 @@ surf2 = ax2.plot_surface(
     xR, yR, zR, cmap="jet", alpha=0.6,
     linewidth=0.5, facecolors=plt.cm.jet(z),
     rcount=30, ccount=30)
+ax2.set_box_aspect(box3d_aspect_ratio)
 surf2.set_edgecolor("k")
 ax2.set_xlim3d(-2, 2)
 ax2.set_ylim3d(-2, 2)
