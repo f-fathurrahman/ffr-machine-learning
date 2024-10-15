2968727aeb2f33270ed2141ab9f1c959414e7431
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-10-15 11:15:01 +0700

    Try to use verbose, not working

--- brunton_data_driven/ch03/test_underdetermined_01.py
+++ brunton_data_driven/ch03/test_underdetermined_01.py
@@ -18,7 +18,7 @@ def L1_norm(x):
 
 constr = ({'type': 'eq', 'fun': lambda x:  Theta @ x - y})
 x0 = np.linalg.pinv(Theta) @ y # initialize with L2 solution
-res = minimize(L1_norm, x0, method='SLSQP',constraints=constr)
+res = minimize(L1_norm, x0, method='SLSQP',constraints=constr, options={'disp': True})
 s_L1 = res.x
 
 # L2 Minimum norm solution s_L2
@@ -26,10 +26,14 @@ s_L2 = np.linalg.pinv(Theta) @ y
 
 fig,axs = plt.subplots(2,2)
 axs = axs.reshape(-1)
-axs[0].plot(s_L1,color='b',linewidth=1.5)
+axs[0].plot(s_L1,color='b',linewidth=1.5, label="s_L1")
 axs[0].set_ylim(-0.2,0.2)
-axs[1].plot(s_L2,color='r',linewidth=1.5)
+axs[0].legend()
+#
+axs[1].plot(s_L2,color='r',linewidth=1.5, label="s_L2")
 axs[1].set_ylim(-0.2,0.2)
+axs[0].legend()
+#
 axs[2].hist(s_L1,bins=np.arange(-0.105,0.105,0.01),rwidth=0.9)
 axs[3].hist(s_L2,bins=np.arange(-0.105,0.105,0.01),rwidth=0.9)
 
